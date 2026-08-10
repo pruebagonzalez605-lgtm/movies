a265795884bb53796b4bb79efeba369977af6351
pruebagonzalez605-lgtm <pruebagonzalez605@gmail.com>
2026-08-09 20:40:38 -0500

    Update movies.js

--- src/scripts/data/movies.js
+++ src/scripts/data/movies.js
@@ -621,4 +621,12 @@ export const MOVIES = [
     tmdbYear: 2021,
     src: "https://github.com/pruebagonzalez605-lgtm/movies/releases/download/1.29/VYS.mp4",
   },
+    {
+    code: "78",
+    title: "La Dama de negro",
+    tmdbTitle: "The Woman in Black",
+    gradient: ["#3d0d0d", "#8a6a1e"],
+    tmdbYear: 2012,
+    src: "",
+  },
 ];
